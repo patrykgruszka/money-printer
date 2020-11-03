130ea1becdf059597de3bfc8395b07415da9b77b
patrykgruszka <patryk.gruszka@protonmail.com>
2020-11-03 22:57:26 +0100

    Use object prototypes and money print method

--- dist/money.js
+++ dist/money.js
@@ -1,91 +1,115 @@
-(function(global, money) {
+(function (global, money) {
     if (typeof exports === 'object' && typeof module !== 'undefined') {
         module.exports = money;
     } else if (typeof define === 'function' && define.amd) {
-        define('money', [], function() {
+        define('money', [], function () {
             return money;
         });
     } else {
         global.money = money;
     }
-})(this, function() {
-    var defaults = {
-        code: 'NONE',
-        precision : 2,
-        decimal: ',',
-        thousand: '&nbsp;',
-        format: '{{value}}'
+})(this, function () {
+
+    var Money = function () {
+        this.currencyList = Money.defaults.currencyList;
     };
 
-    var currencyList = {
-        PLN: {
-            code: 'PLN',
-            thousand: '&nbsp;',
-            decimal: ',',
-            symbol: 'zł',
-            format: '{{value}}&nbsp;{{symbol}}'
-        },
-        EUR: {
-            code: 'EUR',
-            thousand: '&nbsp;',
-            decimal: '.',
-            symbol: '€',
-            format: '{{value}}&nbsp;{{symbol}}'
-        },
-        USD: {
-            code: 'USD',
-            decimal: '.',
-            thousand: ',',
-            symbol: '$',
-            format: '{{symbol}}{{value}}'
-        },
-        GBP: {
-            code: 'GBP',
-            decimal: '.',
-            thousand: ',',
-            symbol: '£',
-            format: '{{symbol}}{{value}}'
-        },
-        JPY: {
-            code: 'JPY',
-            decimal: '.',
-            thousand: ',',
-            symbol: '¥',
-            format: '{{symbol}}{{value}}'
-        },
-        CZK: {
-            code: 'CZK',
-            decimal: '.',
-            thousand: ',',
-            symbol: 'Kč',
-            format: '{{value}}&nbsp;{{symbol}}'
-        },
-        SEK: {
-            code: 'SEK',
-            decimal: '.',
-            thousand: ',',
-            symbol: 'kr',
-            format: '{{value}}&nbsp;{{symbol}}'
+    Money.defaults = {
+        currencyList: {
+            NONE: {
+                code: 'NONE',
+                precision: 2,
+                thousand: ' ',
+                decimal: ',',
+                symbol: 'zł',
+                format: '{{value}}'
+            },
+            PLN: {
+                code: 'PLN',
+                precision: 2,
+                thousand: ' ',
+                decimal: ',',
+                symbol: 'zł',
+                format: '{{value}} {{symbol}}'
+            },
+            EUR: {
+                code: 'EUR',
+                precision: 2,
+                thousand: ' ',
+                decimal: '.',
+                symbol: '€',
+                format: '{{value}} {{symbol}}'
+            },
+            USD: {
+                code: 'USD',
+                precision: 2,
+                decimal: '.',
+                thousand: ',',
+                symbol: '$',
+                format: '{{symbol}}{{value}}'
+            },
+            GBP: {
+                code: 'GBP',
+                precision: 2,
+                decimal: '.',
+                thousand: ',',
+                symbol: '£',
+                format: '{{symbol}}{{value}}'
+            },
+            JPY: {
+                code: 'JPY',
+                precision: 2,
+                decimal: '.',
+                thousand: ',',
+                symbol: '¥',
+                format: '{{symbol}}{{value}}'
+            },
+            CZK: {
+                code: 'CZK',
+                precision: 2,
+                decimal: '.',
+                thousand: ',',
+                symbol: 'Kč',
+                format: '{{value}} {{symbol}}'
+            },
+            SEK: {
+                code: 'SEK',
+                precision: 2,
+                decimal: '.',
+                thousand: ',',
+                symbol: 'kr',
+                format: '{{value}} {{symbol}}'
+            }
         }
     };
 
-    var money = function(value, iso) {
-        iso = iso || '';
+    Money.prototype.print = function (value, iso, nbsp) {
+        iso = iso || 'NONE';
+        nbsp = nbsp || false;
 
-        var currency = currencyList[iso] || {},
+        var currency = this.currencyList[iso] || this.currencyList.NONE,
             number = parseFloat(String(value).replace(',', '.')) || 0,
-            places = currency.precision || defaults.precision,
-            symbol = currency.symbol || iso || defaults.symbol,
-            format = currency.format || defaults.format,
-            decimal = currency.decimal || defaults.decimal,
-            thousand = currency.thousand || defaults.thousand,
+            places = currency.precision,
+            symbol = currency.symbol,
+            format = currency.format,
+            decimal = currency.decimal,
+            thousand = currency.thousand,
             negative = number < 0 ? '-' : '',
-            i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
-            j = (j = i.length) > 3 ? j % 3 : 0;
+            i, j;
 
-        value = negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
-        return format.replace('{{symbol}}', symbol).replace('{{value}}', value);
+        i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "";
+        j = (j = i.length) > 3 ? j % 3 : 0;
+
+        value = negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j)
+            .replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i)
+            .toFixed(places).slice(2) : "");
+
+        value = format
+            .replace('{{symbol}}', symbol)
+            .replace('{{value}}', value);
+
+        return nbsp ? value.split(' ').join('&nbsp;') : value;
     };
 
-    return money;
+    return new Money();
 }());
--- test/moneyTest.js
+++ test/moneyTest.js
@@ -1,56 +1,59 @@
-var _nbsp = function(text) {
-    var separator = '&nbsp;';
-    return text.split(' ').join('&nbsp;');
-};
-
 describe('money', function() {
     it('should be a function', function() {
-        expect(typeof money).toBe('function');
+        expect(typeof money).toBe('object');
+        expect(typeof money.print).toBe('function');
     });
 
     it('should return string', function() {
-        expect(typeof money(0)).toBe('string');
-        expect(typeof money(0, 'PLN')).toBe('string');
-        expect(typeof money(0, '')).toBe('string');
+        expect(typeof money.print(0)).toBe('string');
+        expect(typeof money.print(0, 'PLN')).toBe('string');
+        expect(typeof money.print(0, '')).toBe('string');
     });
 
     it('should return valid money format without currency', function() {
-        expect(money(0)).toBe(_nbsp('0,00'));
-        expect(money(1)).toBe(_nbsp('1,00'));
-        expect(money(10.99)).toBe(_nbsp('10,99'));
-        expect(money(1000.5)).toBe(_nbsp('1 000,50'));
+        expect(money.print(0)).toBe('0,00');
+        expect(money.print(1)).toBe('1,00');
+        expect(money.print(10.99)).toBe('10,99');
+        expect(money.print(1000.5)).toBe('1 000,50');
     });
 
     it('should accept text values with "," as decimal separator', function() {
-        expect(money('1,99')).toBe(_nbsp('1,99'));
+        expect(money.print('1,99')).toBe('1,99');
     });
 });
 
 describe('PLN', function() {
     it('should return currency text', function() {
-        expect(money(0, 'PLN')).toBe(_nbsp('0,00 zł'));
-        expect(money(1, 'PLN')).toBe(_nbsp('1,00 zł'));
-        expect(money(10.99, 'PLN')).toBe(_nbsp('10,99 zł'));
-        expect(money(1000.5, 'PLN')).toBe(_nbsp('1 000,50 zł'));
-        expect(money('1,99', 'PLN')).toBe(_nbsp('1,99 zł'));
+        expect(money.print(0, 'PLN')).toBe('0,00 zł');
+        expect(money.print(1, 'PLN')).toBe('1,00 zł');
+        expect(money.print(10.99, 'PLN')).toBe('10,99 zł');
+        expect(money.print(1000.5, 'PLN')).toBe('1 000,50 zł');
+        expect(money.print('1,99', 'PLN')).toBe('1,99 zł');
     });
 });
 
 describe('EUR', function() {
     it('should return currency text', function() {
-        expect(money(0, 'EUR')).toBe(_nbsp('0.00 €'));
-        expect(money(1, 'EUR')).toBe(_nbsp('1.00 €'));
-        expect(money(10.99, 'EUR')).toBe(_nbsp('10.99 €'));
-        expect(money(1000.5, 'EUR')).toBe(_nbsp('1 000.50 €'));
+        expect(money.print(0, 'EUR')).toBe('0.00 €');
+        expect(money.print(1, 'EUR')).toBe('1.00 €');
+        expect(money.print(10.99, 'EUR')).toBe('10.99 €');
+        expect(money.print(1000.5, 'EUR')).toBe('1 000.50 €');
     });
 });
 
 describe('USD', function() {
     it('should return currency text', function() {
-        expect(money(0, 'USD')).toBe(_nbsp('$0.00'));
-        expect(money(1, 'USD')).toBe(_nbsp('$1.00'));
-        expect(money(10.99, 'USD')).toBe(_nbsp('$10.99'));
-        expect(money(1000.5, 'USD')).toBe(_nbsp('$1,000.50'));
+        expect(money.print(0, 'USD')).toBe('$0.00');
+        expect(money.print(1, 'USD')).toBe('$1.00');
+        expect(money.print(10.99, 'USD')).toBe('$10.99');
+        expect(money.print(1000.5, 'USD')).toBe('$1,000.50');
     });
 });
 
+describe('nbsp', function() {
+    it('should replace spaces with nbsp character', function() {
+        expect(money.print(1, 'EUR', true)).toBe('1.00&nbsp;€');
+        expect(money.print(1000.5, 'EUR', true)).toBe('1&nbsp;000.50&nbsp;€');
+        expect(money.print(1000000.5, 'EUR', true)).toBe('1&nbsp;000&nbsp;000.50&nbsp;€');
+    });
+});
